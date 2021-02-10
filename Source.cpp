660d95d817cfae13247fd5140b4c3a3be719a3e2
kcholdaway <74071575+kcholdaway@users.noreply.github.com>
2021-02-09 19:43:53 -0800

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -5,27 +5,168 @@
 #include <string>
 
 using namespace std;
-int main()
-{
 
-    int now;//number of rows
-    int loops; //nomber of loops done
-    cout << "number of rows" <<endl;
-    cin >> now;
+int main() {
+	string is;
+	int randnum = 0; 
 
-    for (loops = 1; loops <= now; loops++)
-    {
-        for (int x = now - 1; x >= loops; x--)
-        {
-            cout << " ";
-        }
 
-        for (int x = 1; x <= loops; x++)
-        {
-            cout << "* ";
-        }
-        cout << endl;
 
-    }
+	cout << "Welcome to matcher. Here you will be matched" << endl << "enter your initails like this (A. B)"<<endl;
+	cin >> is;
 
+	randnum = rand() % 10;
+	switch (randnum) {
+
+	case 0:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout <<  "   **       **"<< endl;
+		cout <<       "*              *" << endl;
+		cout <<       " *            *" << endl;
+		cout <<       "  *  "<< is <<"+B. Z *." << endl;
+		cout <<       "   *         *" << endl;
+		cout <<       "    *       *" << endl;
+		cout <<       "     *    *" << endl;
+		cout <<       "       * *" << endl;
+		break;
+	case 1:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+K. V *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 2:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+M. C *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 3:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+H. L *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 4:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+O. P *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 5:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+U. S *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 6:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+U. C *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 7:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+I. C *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 8:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+L. Q *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 9:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+Y. M *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+	case 10:
+		system("Color 7C");
+		cout << "    HAPPY VALENTIES DAY" << endl;
+		cout << "   **       **" << endl;
+		cout << "*              *" << endl;
+		cout << " *            *" << endl;
+		cout << "  *  " << is << "+N. O *." << endl;
+		cout << "   *         *" << endl;
+		cout << "    *       *" << endl;
+		cout << "     *    *" << endl;
+		cout << "       * *" << endl;
+
+		break;
+
+
+	default:
+		cout << "error" << endl;
+		break;
+
+	}
+
+	return 0;
 }
